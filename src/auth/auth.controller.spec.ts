37797c16d9d1c961296a0b9885c686acda15a859
sfayman <Sergey_Fayman@bmc.com>
2020-01-03 23:35:51 +0200

    provide env to test files

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,21 +1,18 @@
+// tslint:disable-next-line
+require('dotenv').config();
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersModule } from '../users/users.module';
 import { UsersService } from '../users/users.service';
 import { AuthController } from './auth.controller';
 import { AuthModule } from './auth.module';
 import { AuthService } from './auth.service';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './jwt.strategy';
 
 describe('Auth Controller', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
-    process.env = Object.assign(process.env, {
-      JWT_SECRET: 'value',
-      JWT_EXPIRE: '1ds',
-    });
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [UsersService, AuthService],
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,3 +1,5 @@
+// tslint:disable-next-line
+require('dotenv').config();
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
@@ -7,12 +9,6 @@ import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
-
-  process.env = Object.assign(process.env, {
-    JWT_SECRET: 'value',
-    JWT_EXPIRE: '1ds',
-  });
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
