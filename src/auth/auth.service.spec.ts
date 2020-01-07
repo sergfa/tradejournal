6ca92806414ea373de4e6528472738a76dda0d44
sfayman <Sergey_Fayman@bmc.com>
2020-01-07 13:58:28 +0200

    implement auth tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,9 +1,12 @@
 // tslint:disable-next-line
 require('dotenv').config();
 import { AzureTableStorageModule } from '@nestjs/azure-database';
+import { INestApplication } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserDTO } from 'src/users/user.model';
+import * as request from 'supertest';
 import { CommonModule } from '../common/common.module';
 import { UserEntity } from '../users/user.entity';
 import { UsersModule } from '../users/users.module';
@@ -11,7 +14,17 @@ import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let usersService: UsersService;
+  let app: INestApplication;
+  let isUserCreated = false;
+
+  const userDTO: UserDTO = {
+    username: 'testuser12345',
+    password: 'very_secret_password',
+    email: 'test@email.com',
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
@@ -30,10 +43,67 @@ describe('AuthService', () => {
       providers: [AuthService, UsersService],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  beforeEach(async () => {
+    if (isUserCreated) {
+      return;
+    }
+    const users = await usersService.getAll();
+    users.forEach(async user => {
+      await usersService.deleteUser(user.RowKey);
+    });
+
+    await request(app.getHttpServer())
+      .post('/users')
+      .send(userDTO)
+      .set('Accept', 'application/json');
+    isUserCreated = true;
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  it('should succefully validate user', async () => {
+    const userEntity = await authService.validateUser(
+      userDTO.username,
+      userDTO.password,
+    );
+    expect(userEntity).toBeTruthy();
+  });
+
+  it('should fail to validate user with wrong password', async () => {
+    const userEntity = await authService.validateUser(
+      userDTO.username,
+      userDTO.password.toUpperCase(),
+    );
+    expect(userEntity).toBeFalsy();
+  });
+
+  it('should fail to validate unregistred user', async () => {
+    const userEntity = await authService.validateUser(
+      'dummy-user',
+      userDTO.password,
+    );
+    expect(userEntity).toBeFalsy();
+  });
+
+  it('should succefully login user', async () => {
+    const userEntity = await authService.validateUser(
+      userDTO.username,
+      userDTO.password,
+    );
+    const accessInfo = await authService.login(userEntity);
+    expect(accessInfo).toBeTruthy();
+    expect(accessInfo.token).toBeTruthy();
   });
 });
